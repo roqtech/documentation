b5a5cab2ee8f1ad02338fa2e130d33d60b3693b8
Fabian Wesner <fabian.wesner@roq.tech>
2022-12-19 18:29:58 +0100

    added content

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2da0543f98c557d4"; }
+/******/ 		__webpack_require__.h = function() { return "d43238ff0bb847b1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
