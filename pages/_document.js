8581141971ab4a57144c47ebfeec8e6a153ba507
Equan P. <equan.p@gmail.com>
2023-11-03 09:14:09 +0700

    install hotjar

--- pages/_document.js
+++ pages/_document.js
@@ -8,32 +8,46 @@ export default class AnalyticsHead extends Document {
   render() {
     return (
       <Html lang="en" translate="no">
-       <Head>
-  {/* Other meta tags and links */}
-  <script
-    type="text/javascript"
-    dangerouslySetInnerHTML={{
-      __html: `
-      (function (k) {
-        (function (w, d, s, o, f, js, fjs) {
-          w[o] =
-            w[o] ||
-            function () {
-              (w[o].q = w[o].q || []).push(arguments);
-            };
-          (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
-          js.id = o;
-          js.src = f;
-          js.async = 1;
-          fjs.parentNode.insertBefore(js, fjs);
-        })(window, document, 'script', '_hw', 'https://app.threado.com/widget.js');
-        _hw('init', { appId: 68157, minimized: true, token: k });
-        _hw('modal', { appId: 68157, instance: 'chat-modal', minimized: true, token: k });
-      })('230731135109822_9U>iB');
-    `,
-    }}
-  />
-</Head>
+        <Head>
+          {/* Other meta tags and links */}
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function (k) {
+                  (function (w, d, s, o, f, js, fjs) {
+                    w[o] =
+                      w[o] ||
+                      function () {
+                        (w[o].q = w[o].q || []).push(arguments);
+                      };
+                    (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
+                    js.id = o;
+                    js.src = f;
+                    js.async = 1;
+                    fjs.parentNode.insertBefore(js, fjs);
+                  })(window, document, 'script', '_hw', 'https://app.threado.com/widget.js');
+                  _hw('init', { appId: 68157, minimized: true, token: k });
+                  _hw('modal', { appId: 68157, instance: 'chat-modal', minimized: true, token: k });
+                })('230731135109822_9U>iB');
+              `,
+            }}
+          />
+          {process.env.IS_PRODUCTION && (
+            <script
+              type='text/javascript'
+              dangerouslySetInnerHTML={{
+                __html: `ction(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3436899,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+              }}
+            />)}
+        </Head>
 
         <body>
           <Main />
