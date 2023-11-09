c12bfe2765a5e6d49342b38eaaf12bdffaefa4eb
Equan P. <equan.p@gmail.com>
2023-11-09 19:04:16 +0700

    Update Hotjar script with new ID.

--- pages/_document.js
+++ pages/_document.js
@@ -38,13 +38,13 @@ export default class AnalyticsHead extends Document {
             type='text/javascript'
             dangerouslySetInnerHTML={{
               __html: `(function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:3436899,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+                      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                      h._hjSettings={hjid:3729216,hjsv:6};
+                      a=o.getElementsByTagName('head')[0];
+                      r=o.createElement('script');r.async=1;
+                      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                      a.appendChild(r);
+                  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
             }}
           />
         </Head>
